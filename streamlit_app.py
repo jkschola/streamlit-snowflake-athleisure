c49d5388fc90e2dbbdcee92a509c93ef6cefa7d4
Schola Janvier <90581570+jkschola@users.noreply.github.com>
2025-03-09 17:05:38 +0100

    Update streamlit_app.py
    Set up the Streamlit app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import pandas as pd
+
+# Set up the Streamlit app title
+st.title("Zena's Amazing Athleisure Catalog")
